6eb64ae848282abbdc05be9df44e12db14b850e3
emlivo1 <48747184+emlivo1@users.noreply.github.com>
2021-03-24 18:23:53 +0100

    Dodana promjena backgrounda

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,12 +3,15 @@ package sample;
 import javafx.event.ActionEvent;
 
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Controller {
     public Label labela;
+    public GridPane back;
     public void sayhello(ActionEvent actionEvent) {
         labela.setText("Hello World!");
+        back.setStyle("-fx-background-color:red");
 
     }
 }
